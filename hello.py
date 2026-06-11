a280c201407c4a01c2f19a5f59d4354c00bba1ff
shelarshriniwas <shelar.shriniwas@gmail.com>
2026-06-12 00:02:27 +0530

    added

--- hello.py
+++ hello.py
@@ -1,9 +1,2 @@
 print("This is python program for poll scm")
 print("This is web hook of github with jenkins ")
-print("This is python program for poll scm")
-print("This is web hook of github with jenkins ")
-print("This is python program for poll scm")
-print("This is web hook of github with jenkins ")
-print("This is python program for poll scm")
-print("This is web hook of github with jenkins ")
-https://wispy-whimsical-sprawl.ngrok-free.dev/github-webhook/
