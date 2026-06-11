4100f7ead4cfde25391013c65cbbffad75e090ad
shelarshriniwas <shelar.shriniwas@gmail.com>
2026-06-11 23:52:35 +0530

    modify add 2 new  line hello.py content

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
 print("This is python program for poll scm")
 print("This is web hook of github with jenkins ")
-print("This is python program for poll scm")
-print("This is web hook of github with jenkins ")
